7c1944df2676201b736973362755743e6c09e41e
Harsh Vats <vatsvatsharsh2000@gmail.com>
2020-12-27 19:26:09 +0530

    made headless

--- utils/insta-client.js
+++ utils/insta-client.js
@@ -46,15 +46,19 @@ class InstagramClient {
 
   async getVideoDownloadLink(url) {
     if (!this.browser) throw new Error('Browser not started');
-
+    console.log('line 49');
     const page = await this.browser.newPage();
+    console.log('line 51');
     await page.setUserAgent(
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
     );
+    console.log('line 55');
     await page.goto(url);
+    console.log('line 57');
 
     // Wait until the page got completly renderer
     await page.waitForSelector('div');
+    console.log('line 61');
 
     // setTimeout(async () => {
     const link = await page.evaluate(() => {
